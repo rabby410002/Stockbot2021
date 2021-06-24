26f65e16d7c5fa23851cc632f221d44989ab2246
peihsuan <rabby410002@gmail.com>
2021-06-24 15:24:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,12 +35,31 @@ def callback():
         abort(400)
 
     return 'OK'
-
+'''
 #訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
+'''    
+    
+#訊息傳遞區塊
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    ### 抓到顧客的資料 ###
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id #使用者ID
+    usespeak=str(event.message.text) #使用者講的話
+    if re.match('[0-9]{4}[<>][0-9]',usespeak): # 先判斷是否是使用者要用來存股票的
+        line_bot_api.push_message(uid, TextSendMessage('測試儲存'))
+        mongodb.write_user_stock_fountion(stock=usespeak[0:4], bs=usespeak[4:5], price=usespeak[5:])
+        line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+'已經儲存成功'))
+        return 0
+    
+    elif re.match('刪除[0-9]{4}',usespeak): # 刪除存在資料庫裡面的股票
+        mongodb.delete_user_stock_fountion(stock=usespeak[2:])
+        line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經刪除成功'))
+        return 0
 
 #主程式
 import os
